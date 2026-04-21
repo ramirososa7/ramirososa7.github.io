35d12ff0dce347be69bb684019a1c72915f9f04e
Ramiro <ramironahuel@outlook.es>
2026-04-21 11:22:20 -0300

    traduccion terminada

--- Java.js
+++ Java.js
@@ -4,41 +4,41 @@ const textos ={
         exper:"Experiencia",
         proy:"Proyectos",
         titulo: "Desarrollador en formacion",
-        descripcion:"Estudiante de Analista de Sistemas",
+        descripcion:"Estudiante de Analista de Sistemas| HTML,CSS,JavaScript,Python,SQL,GIT",
         descarga:"Descargar CV",
         exp:"Experiencia Laboral",
-        tiempo:"Actualidad",
+        tiempo:"2022-Actualidad",
         busqueda:"Buscando mi primera oportunidad en el mundo IT",
         actualmente:"Actualmente formándome como Analista de Sistemas y desarrollando proyectos propios para mejorar mis habilidades.",
         proyectos:"Proyectos Personales",
         portfolio:"Portfolio Digital",
         descripciontrabajo:"Portfolio web responsive para mostrar mis proyectos y habilidades como desarrollador en formación",
         codigo:"Código",
-        footer:"Todos los derechos reservados"
+        footer:"\u00A9 2026 Ramiro Sosa -Todos los derechos reservados"
     },
     en: {
         inicio:"Home",
         exper:"Experience",
         proy:"Projects",
         titulo:"Junior Developer",
-        descripcion:"Systems Analyst Student",
+        descripcion:"Systems Analyst Student| HTML,CSS,JavaScript,Python,SQL,GIT",
         descarga:"Download CV",
         exp:"Work Experience",
-        tiempo:"Present",
+        tiempo:"2022-Present",
         busqueda: "Looking for my firste opportunity in the world IT",
         actualmente:"I am currently training to be Systems Analyst and developing my own projects to improve my skills",
         proyectos:"Personal Projects",
         portfolio:"Digital Portfolio",
         descripciontrabajo:"Responsive web portfolio to showcase my projects and skills as a developer in training",
         codigo:"Code",
-        footer:"All rights reserved"
+        footer:"\u00A9 2026 Ramiro Sosa - All rights reserved"
     }
-}
+};
 
 function cambiarIdioma(idioma){
     const elementos = document.querySelectorAll("[data-key]");
-    elementos.forEach(el => {
-        const key = el.getAttribute("data-key");
-        e1.textContent = textos[idioma][key];
+    elementos.forEach(button => {
+        const key = button.getAttribute("data-key");
+        button.textContent = textos[idioma][key];
     });
 }
